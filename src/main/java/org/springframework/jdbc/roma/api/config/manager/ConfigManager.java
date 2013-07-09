a0e4894d9d4359d148136016b52e55cf81a7c357
unknown <Asus@Asus-Bilgisayar.(none)>
2013-07-09 19:47:23 +0300

    initial commit

---
+++ src/main/java/org/springframework/jdbc/roma/api/config/manager/ConfigManager.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jdbc.roma.api.config.manager;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.springframework.jdbc.roma.api.config.provider.ConfigProvider;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperBlobFieldConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperClassConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperClobFieldConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperEnumFieldConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperFieldConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperObjectFieldConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperTimestampFieldConfig;
+
+/**
+ * @author Serkan ÖZAL
+ */
+public interface ConfigManager {
+
+	public void addConfigProvider(ConfigProvider configProvider);
+	public void removeConfigProvider(ConfigProvider configProvider);
+	public List<ConfigProvider> getAllConfigProviders();
+	
+	public RowMapperFieldConfig getRowMapperFieldConfig(Class<?> clazz, String fieldName);
+	public RowMapperObjectFieldConfig getRowMapperObjectFieldConfig(Class<?> clazz, String fieldName);
+	public RowMapperEnumFieldConfig getRowMapperEnumFieldConfig(Class<?> clazz, String fieldName);
+	public RowMapperClobFieldConfig getRowMapperClobFieldConfig(Class<?> clazz, String fieldName);
+	public RowMapperBlobFieldConfig getRowMapperBlobFieldConfig(Class<?> clazz, String fieldName);
+	public RowMapperTimestampFieldConfig getRowMapperTimestampFieldConfig(Class<?> clazz, String fieldName);
+	public RowMapperClassConfig getRowMapperClassConfig(Class<?> clazz);
+	
+	public RowMapperFieldConfig getRowMapperFieldConfig(Field field);
+	public RowMapperObjectFieldConfig getRowMapperObjectFieldConfig(Field field);
+	public RowMapperEnumFieldConfig getRowMapperEnumFieldConfig(Field field);
+	public RowMapperClobFieldConfig getRowMapperClobFieldConfig(Field field);
+	public RowMapperBlobFieldConfig getRowMapperBlobFieldConfig(Field field);
+	public RowMapperTimestampFieldConfig getRowMapperTimestampFieldConfig(Field field);
+	
+}
---
+++ src/main/java/org/springframework/jdbc/roma/api/domain/builder/config/RowMapperObjectFieldConfigBuilder.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jdbc.roma.api.domain.builder.config;
+
+import java.lang.reflect.Field;
+
+import org.springframework.jdbc.roma.api.domain.builder.Builder;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperCustomProviderConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperImplementationProviderConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperObjectFieldConfig;
+import org.springframework.jdbc.roma.api.domain.model.config.RowMapperSpringProviderConfig;
+
+/**
+ * @author Serkan ÖZAL
+ */
+public class RowMapperObjectFieldConfigBuilder implements Builder<RowMapperObjectFieldConfig> {
+
+	private Field field;
+	private RowMapperSpringProviderConfig rowMapperSpringProviderConfig;
+	private RowMapperImplementationProviderConfig rowMapperImplementationProviderConfig;
+	private RowMapperCustomProviderConfig rowMapperCustomProviderConfig;
+	private Class<?> fieldType;
+	private boolean lazy = false;
+	
+	@Override
+	public RowMapperObjectFieldConfig build() {
+		RowMapperObjectFieldConfig config = new RowMapperObjectFieldConfig();
+		config.setField(field);
+		config.setRowMapperSpringProviderConfig(rowMapperSpringProviderConfig);
+		config.setRowMapperImplementationProviderConfig(rowMapperImplementationProviderConfig);
+		config.setRowMapperCustomProviderConfig(rowMapperCustomProviderConfig);
+		config.setFieldType(fieldType);
+		config.setLazy(lazy);
+		return config;
+	}
+	
+	public RowMapperObjectFieldConfigBuilder field(Field field) {
+		this.field = field;
+		return this;
+	}
+	
+	public RowMapperObjectFieldConfigBuilder rowMapperSpringProviderConfig(
+			RowMapperSpringProviderConfig rowMapperSpringProviderConfig) {
+		this.rowMapperSpringProviderConfig = rowMapperSpringProviderConfig;
+		return this;
+	}
+	
+	public RowMapperObjectFieldConfigBuilder rowMapperImplementationProviderConfig(
+			RowMapperImplementationProviderConfig rowMapperImplementationProviderConfig) {
+		this.rowMapperImplementationProviderConfig = rowMapperImplementationProviderConfig;
+		return this;
+	}
+	
+	public RowMapperObjectFieldConfigBuilder rowMapperCustomProviderConfig(
+			RowMapperCustomProviderConfig rowMapperCustomProviderConfig) {
+		this.rowMapperCustomProviderConfig = rowMapperCustomProviderConfig;	
+		return this;
+	}
+	
+	public RowMapperObjectFieldConfigBuilder fieldType(Class<?> fieldType) {
+		this.fieldType = fieldType;
+		return this;
+	}
+	
+	public RowMapperObjectFieldConfigBuilder lazy(boolean lazy) {
+		this.lazy = lazy;
+		return this;
+	}
+
+}
